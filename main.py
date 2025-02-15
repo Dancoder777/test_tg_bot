b548cc4da12f3eeee38c47a1126a1d247c320d58
Абышев Данил <@step.kz>
2025-02-15 10:02:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+x = 0
+while x < 5:
+    print(x)
+    x +=1
+
+# Задание 2
+num = [1, 2, 3, 4, 5]
+a = (num[0]+num[1]+num[2]+num[3]+num[4]) 
+
+print(a)
+
+# Задание 3
+for i in range (0,56):
+    if i %2 == 0:
+        print(i, end=" ") 
+
+# Задание 4
+
+
+
