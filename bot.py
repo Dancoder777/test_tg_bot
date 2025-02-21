e9f3f9a36b78d8958f62bad7cd559c0d8627b4ce
Dancoder_777 <gifacool2000@gmail.com>
2025-02-21 18:43:18 +0500

    код с объяснением

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+import asyncio # это для запуска бота
+from aiogram import Bot, Dispatcher, types # это библиотека для создания Tg-бота.
 from aiogram.types import (
     ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 )
+#ReplyKeyboardMarkup - инициальзация обычной клавиатуры
+# KeyboardButton - кнопка для обычной клавиатуры на телефоне 
+# InlineKeyboardMarkup - инлайн клавиатуры
+# InlineKeyboardButton - кнопка для инлайн клавиатуры
+
 from aiogram.filters import Command
-from aiogram.client.default import DefaultBotProperties
-from api import Token
+from aiogram.client.default import DefaultBotProperties # нужно для указания стандартных настроек бота
+from api import Token #
 
 # Создаем объект - бот и диспетчер
 bot = Bot(token=Token, default=DefaultBotProperties(parse_mode="HTML"))
@@ -22,21 +27,21 @@ main_keyboard = ReplyKeyboardMarkup(
 # Инлайн клавиатуры
 inline_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text="Перейти на сайт", url="http://example.com")],
-        [InlineKeyboardButton(text="Нажми", callback_data="button_click")]
+        [InlineKeyboardButton(text="Перейти на сайт", url="http://example.com")], # создаём кнопку перейти на сайт 
+        [InlineKeyboardButton(text="Нажми", callback_data="button_click")]# создаём кнопку перейти на сайт 
     ]
 )
 
-@dp.message(Command("start"))
+@dp.message(Command("start")) # эта функция будет выполнятся, при вводе данной команды
 async def start(message: types.Message):
-    await message.answer("Привет! Я тестовый бот <b>test</b>", reply_markup=main_keyboard)
+    await message.answer("Привет! Я тестовый бот <b>test</b>", reply_markup=main_keyboard) # отправляет пользователю сообщение с текстом "Привет! Я тестовый бот"
 
-@dp.message(lambda message: message.text == "Привет!")
+@dp.message(lambda message: message.text == "Привет!") # эта функция будет выполнятся, когда пользователь напишет "Привет!"
 async def hello(message: types.Message):
-    await message.answer("Привет!!! Как дела?", reply_markup=inline_keyboard)
+    await message.answer("Привет!!! Как дела?", reply_markup=inline_keyboard) # отправляет пользователю сообщение с текстом "Привет!!! Как дела?"
 
-async def main():
-    await dp.start_polling(bot)
+async def main(): # основная функция, которая запускает нашего бота
+    await dp.start_polling(bot) # бот отслеживает новые команды от пользователя
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__": # данная команда проверяет, что бот запущен напрямую пользователем
+    asyncio.run(main()) # Запускает функцию, которая включает бота
